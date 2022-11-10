0baf323a8c7249ab105be3d7ce05fd38659e3f4b
anastasiia-matushenko <muzaam07@gmail.com>
2022-11-10 15:19:26 +0200

    added auth

---
+++ src/redux/auth/authSelectors.js
@@ -0,0 +1,7 @@
+export const selectToken = state => state.auth.token;
+export const selectName = state => state.auth.user.name;
+export const selectEmail = state => state.auth.user.email;
+export const selectIsAuth = state => state.auth.isAuth;
+export const selectIsLoadingUser = state => state.auth.isLoading;
+export const selectIsFetching = state => state.auth.isFetching;
+export const selectAuthError = state => state.auth.error;
---
+++ src/redux/auth/authSlice.js
@@ -0,0 +1,82 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { register, login, logout, fetchCurrentUser } from './authOperations';
+
+const initialState = {
+  user: { name: '', email: '' },
+  token: null,
+  isLoading: false,
+  isAuth: false,
+  error: null,
+  isFetching: false,
+};
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const handlePending = state => {
+  state.isLoading = true;
+  state.error = null;
+};
+
+const handleRejected = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = payload;
+};
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  extraReducers: {
+    [register.pending]: handlePending,
+    [login.pending]: handlePending,
+    [logout.pending]: handlePending,
+
+    [register.rejected]: handleRejected,
+    [login.rejected]: handleRejected,
+    [logout.rejected]: handleRejected,
+
+    [register.fulfilled]: (state, { payload: { user, token } }) => {
+      state.token = token;
+      state.user = user;
+      state.isAuth = true;
+      state.isLoading = false;
+    },
+    [login.fulfilled]: (state, { payload: { user, token } }) => {
+      state.token = token;
+      state.user = user;
+      state.isAuth = true;
+      state.isLoading = false;
+    },
+    [logout.fulfilled]: state => {
+      state.token = null;
+      state.user = { name: '', email: '' };
+      state.isAuth = false;
+      state.isLoading = false;
+    },
+    [fetchCurrentUser.pending]: state => {
+      state.isLoading = true;
+      state.isFetching = true;
+    },
+    [fetchCurrentUser.fulfilled]: (state, { payload }) => {
+      state.user = payload;
+      state.isAuth = true;
+      state.isLoading = false;
+      state.isFetching = false;
+    },
+    [fetchCurrentUser.rejected]: (state, { payload }) => {
+      state.error = payload;
+      state.isLoading = false;
+      state.isFetching = false;
+    },
+  },
+});
+
+export const authPersistedReducer = persistReducer(
+  authPersistConfig,
+  authSlice.reducer
+);

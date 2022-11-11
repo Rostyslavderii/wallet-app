42de6de3ff470d611ed278b8e899fb772022ad99
Лілія <lily.fed85@gmail.com>
2022-11-11 08:52:52 +0200

    global styles: normalize and reset

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import theme from './utils/theme';
 import { persistor, store } from 'redux/store';
+import { GlobalStyle } from './utils/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Provider store={store}>
           <PersistGate persistor={persistor}>
             <App />
+            <GlobalStyle />
           </PersistGate>
         </Provider>
       </ThemeProvider>

6ef683fe716a093faa8a0517272ff80851eacc19
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2022-11-11 15:52:05 +0200

    add isModalAddTransactionOpen state

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,18 +7,25 @@ import {
 } from 'redux/transactions/transactionSelectors';
 import { TransactionTable } from 'components/TransactionTable/TransactionTable';
 import { AddTransactionBtn } from 'components/AddTransaction/AddTransactionBtn';
+import { useState } from 'react';
 
 const HomePage = () => {
   const transactions = useSelector(selectTransaction);
   const categories = useSelector(selectCategories);
   const dispatch = useDispatch();
+
+  const [isModalAddTransactionOpen, setIsModalAddTransactionOpen] =
+    useState(false);
   useEffect(() => {
     dispatch(getCategories());
   }, [dispatch]);
   return (
     <>
+      {isModalAddTransactionOpen && 'ModalAddTransactionOpen'}
       <TransactionTable transactions={transactions} categories={categories} />
-      <AddTransactionBtn />
+      <AddTransactionBtn
+        onClick={() => setIsModalAddTransactionOpen(!isModalAddTransactionOpen)}
+      />
     </>
   );
 };

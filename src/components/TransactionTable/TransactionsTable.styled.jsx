eb1a5190d99082ec5ea12c376de9ade57b6e223b
Лілія <lily.fed85@gmail.com>
2022-11-12 10:57:58 +0200

    add table styles for mobile

--- src/components/TransactionTable/TransactionsTable.styled.jsx
+++ src/components/TransactionTable/TransactionsTable.styled.jsx
@@ -1,6 +1,18 @@
 import styled from 'styled-components';
 
+const switchColor = p => {
+  switch (p.type) {
+    case 'INCOME':
+      return p.theme.colors.btGreen;
+    case 'EXPENSE':
+      return p.theme.colors.btPink;
+    default:
+      return p.theme.colors.black;
+  }
+};
+
 export const Table = styled.table`
+  margin: 0 auto;
   border-collapse: collapse;
   font-family: ${p => p.theme.ff.familyExo};
   color: ${p => p.theme.colors.black};
@@ -33,37 +45,55 @@ export const TableHeading = styled.th`
 `;
 
 export const TrData = styled.tr`
+  @media screen and (max-width: 767px) {
+    display: flex;
+    flex-direction: column;
+    &:not(:last-child) {
+      margin-bottom: 8px;
+    }
+    background-color: ${p => p.theme.colors.white};
+    border-radius: 10px;
+    border-left: 5px solid ${switchColor};
+  }
+
   position: relative;
   font-size: 16px;
-
-  &:not(:last-child) {
-    &::after {
-      content: '';
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      border: 1px solid #dcdcdf;
-      box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
+  @media screen and (min-width: 768px) {
+    &:not(:last-child) {
+      &::after {
+        content: '';
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        border: 1px solid #dcdcdf;
+        box-shadow: 0px 1px 0px rgba(255, 255, 255, 0.6);
+      }
     }
   }
 `;
 
 export const StyledTd = styled.td`
+  @media screen and (max-width: 767px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &:not(:last-child) {
+      border-bottom: 1px solid #dcdcdf;
+    }
+  }
   padding-left: 20px;
   padding-right: 20px;
   height: 52px;
   line-height: calc(18 / 16);
-  color: ${p => {
-    switch (p.type) {
-      case 'INCOME':
-        return p.theme.colors.btGreen;
-      case 'EXPENSE':
-        return p.theme.colors.btPink;
-      default:
-        return p.theme.colors.black;
-    }
-  }};
+  color: ${switchColor};
   font-weight: ${p => (p.type ? '700' : '400')};
   text-align: ${p => (p.left ? 'left' : 'center')};
+`;
+
+export const DataHeading = styled.span`
+  color: ${p => p.theme.colors.black};
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 1.5;
 `;

003eb489d26af1ea5f8bc2d7675b9e615171878e
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-11-13 20:47:11 +0200

    go

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -2,6 +2,32 @@ import styled from 'styled-components';
 import { Watch } from 'react-loader-spinner';
 
 export const StyledLoader = styled(Watch)`
+&:after {
+    animation-name: event;
+  animation-duration: 300ms;
+  /* animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-direction: alternate; */
+  }
+  &:before {
+  animation-name: event;
+  animation-duration: 3000ms;
+
+  }
+
+  @keyframes event {
+  0% {
+    opacity: 0;
+  }
+
+  50% {
+    opacity: 0.5;
+  }
+
+  100% {
+    opacity: 1;
+  }
+  }
 
 `;
 

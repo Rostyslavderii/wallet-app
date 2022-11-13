1f85a5bdaf8ad6c27d09dc12bed2e7d344822b1b
Lluppiter <107683409+Lluppiter@users.noreply.github.com>
2022-11-13 21:49:01 +0300

    add styled

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename="/wallet-app/">
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <PersistGate persistor={persistor}>

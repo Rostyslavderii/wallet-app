2c5ebc33379f3042a0b7933312005ddc44d0db18
Лілія <lily.fed85@gmail.com>
2022-11-13 18:29:14 +0200

    refact currency styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -9,6 +9,7 @@ import { Balance } from 'components/Balance/Balance';
 import { AddTransactionBtn } from 'components/AddTransaction/AddTransactionBtn';
 import { ModalWindowWraper } from 'components/ModalWindowWraper/ModalWindowWraper';
 import { EditModal } from 'components/EditModal/EditModal';
+import { Wrapper } from './HomePage.styled';
 
 const HomePage = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -30,7 +31,7 @@ const HomePage = () => {
   };
 
   return (
-    <>
+    <Wrapper>
       {isMobile && <Balance />}
       <TransactionTable
         transactions={transactions}
@@ -54,7 +55,7 @@ const HomePage = () => {
           )}
         </ModalWindowWraper>
       )}
-    </>
+    </Wrapper>
   );
 };
 

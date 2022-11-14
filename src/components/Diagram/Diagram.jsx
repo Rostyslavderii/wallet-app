184229bd9ff5cf5ad10b56855b7f09428b90287d
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2022-11-14 20:49:54 +0200

    add styles

--- src/components/Diagram/Diagram.jsx
+++ src/components/Diagram/Diagram.jsx
@@ -4,53 +4,45 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTransactionsSummary } from 'redux/transactionsSummary/trSummaryOperations';
 import {
-    selectError,
-    selectIsLoading,
-    selectTrSummary,
+  selectError,
+  selectIsLoading,
+  selectTrSummary,
 } from 'redux/transactionsSummary/trSummarySelectors';
 import { ChartBox, Box, Title, Wrapper } from './Diagram.styled';
 
 export const Diagram = () => {
-    const [month, setMonth] = useState('');
-    const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+  const [year, setYear] = useState('');
 
-    const trSummary = useSelector(selectTrSummary);
-    const isLoading = useSelector(selectIsLoading);
-    const error = useSelector(selectError);
-    const dispatch = useDispatch();
+  const trSummary = useSelector(selectTrSummary);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchTransactionsSummary = ({ month, year }) => {
-            dispatch(getTransactionsSummary({ month, year }));
-        };
-
-        if (month && year) {
-            fetchTransactionsSummary({ month, year });
-        }
-
-    }, [month, year, dispatch]);
-
-    const handleChange = ({ target: { name, value } }) => {
-        switch (name) {
-            case 'month':
-                setMonth(value);
-                break;
-            case 'year':
-                setYear(value);
-                break;
-            default:
-                return;
-        }
+  useEffect(() => {
+    const fetchTransactionsSummary = ({ month, year }) => {
+      dispatch(getTransactionsSummary({ month, year }));
     };
 
-    return (
-        <Wrapper>
-            <Title>Statistics</Title>
-            <Box>
-                <ChartBox>{!isLoading && trSummary && <Chart trSummary={trSummary} />}</ChartBox>
-                <StatisticTabel handleChange={handleChange} trSummary={trSummary} />
-                {error && <p>{error}</p>}
-            </Box>
-        </Wrapper>
-    );
+    if (month && year) {
+      fetchTransactionsSummary({ month, year });
+    }
+  }, [month, year, dispatch]);
+
+  return (
+    <Wrapper>
+      <Title>Statistics</Title>
+      <Box>
+        <ChartBox>
+          {!isLoading && trSummary && <Chart trSummary={trSummary} />}
+        </ChartBox>
+        <StatisticTabel
+          setYear={setYear}
+          setMonth={setMonth}
+          trSummary={trSummary}
+        />
+        {error && <p>{error}</p>}
+      </Box>
+    </Wrapper>
+  );
 };

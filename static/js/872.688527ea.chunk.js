1592fae71e8c2b2bd4cd3b621b77da3d0bd51384
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-11-16 18:04:25 +0000

    Deploying to gh-pages from @ Rostyslavderii/wallet-app@a572d5496ecbc809f6f79a6b1e8beccdc92412be 🚀

--- static/js/872.688527ea.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwallet_app=self.webpackChunkwallet_app||[]).push([[872],{3872:function(e,a,l){l.r(a);var n=l(3266),t=l(7779),u=l(184);a.default=function(){return(0,u.jsx)(t.im,{children:(0,u.jsx)(n.F,{})})}}}]);
-//# sourceMappingURL=872.688527ea.chunk.js.map
---
+++ static/js/872.e6a3b6df.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwallet_app=self.webpackChunkwallet_app||[]).push([[872],{3872:function(e,a,l){l.r(a);var n=l(6191),t=l(7779),u=l(184);a.default=function(){return(0,u.jsx)(t.im,{children:(0,u.jsx)(n.F,{})})}}}]);
+//# sourceMappingURL=872.e6a3b6df.chunk.js.map

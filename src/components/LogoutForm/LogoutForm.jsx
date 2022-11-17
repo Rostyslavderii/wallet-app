995377529d2fbbe7c8e1b9f8f2b41bd3be76e6cd
Лілія <lily.fed85@gmail.com>
2022-11-17 16:08:49 +0200

    add close modal by escape

--- src/components/LogoutForm/LogoutForm.jsx
+++ src/components/LogoutForm/LogoutForm.jsx
@@ -1,11 +1,24 @@
 import { Button } from 'components/Button/Button';
 import { ModalTitle } from 'components/ModalAddTransaction/ModalAddTransaction.styled';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { logout } from 'redux/auth/authOperations';
 import { Wrapper, Backdrop } from './Logout.styled';
 
 export const LogoutForm = ({ closeModalFunc }) => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const closeModal = e => {
+      if (e.key === 'Escape') {
+        closeModalFunc();
+      }
+    };
+    window.addEventListener('keydown', closeModal);
+    return () => {
+      window.removeEventListener('keydown', closeModal);
+    };
+  }, [closeModalFunc]);
   return (
     <>
       <Backdrop>
